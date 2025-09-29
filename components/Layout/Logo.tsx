f03412ba1d389d5b7cf1191c73397447d5ecfee0
Momen Sharf2 <99686640+MomenSharf@users.noreply.github.com>
2025-09-29 07:43:37 +0000

    ...

--- components/Layout/Logo.tsx
+++ components/Layout/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <div className="flex items-center gap-1">
       <Icons.logo className="w-10 h-10 fill-primary" />
-      <h1 className="text-lg font-medium">Planora</h1>
+      <h1 className="text-lg font-medium">Planoraa</h1>
     </div>
   );
 }
